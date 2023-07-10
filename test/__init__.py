afb1c44cc3f0655af875ebf3af82fb18058aee1e
tnalsdh21 <79125278+tnalsdh21@users.noreply.github.com>
2023-07-10 16:24:00 +0900

    add orm

--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
+#db에 대한 실제 내용이 작성되어있는 config(db+원격저장소+ 암호화된 ...)
+import config 
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
 
-    
-
+    db.init_app(app)
+    migrate.init_app(app,db)
     from views import main_views
 
     app.register_blueprint(main_views.bp)
